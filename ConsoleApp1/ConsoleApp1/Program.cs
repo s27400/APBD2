d1f0a2e155567019533810217164497f557af247
s27400 <s27400@pjwstk.edu.pl>
2024-03-11 16:33:29 +0100

    Average Added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 class Program
 {
+    public static double Avg(int[] tab)
+    {
+        double res = 0;
+        int counter = 0;
+        foreach (int a in tab)
+        {
+            counter++;
+            res += a;
+        }
+
+        res = res / counter;
+        return res;
+    }
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
 
         int[] tbl = { 1, 2, 3, 4 };
-
-        foreach (int num in tbl)
-        {
-            Console.WriteLine(num);
-        }
+        
+        Console.WriteLine(Avg(tbl));
+        
+        
     }
 }
